f6932cc75170a49e577b722cf31aec52fff0cd3d
Brady Herbert <brady@bradyh.com>
2014-04-13 21:42:09 -0400

    named start page

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.send('Ailanthus: Continuous integration with Heroku');
 });
 
 var port = Number(process.env.PORT || 5000);
